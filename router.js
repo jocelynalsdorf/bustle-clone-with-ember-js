1415b79d44a6722b852062123653d5d59d640d42
david garber & jocelyn alsdorf <student@epicodus.com>
2015-06-08 12:28:06 -0700

    add initial files, nav bar.

---
+++ router.js
@@ -0,0 +1,12 @@
+Bustle.Router.map(function() {
+  this.resource('homepage', {path: '/'});
+  this.resource('news');
+  this.resource('entertainment');
+  this.resource('fashion');
+  this.resource('books');
+  this.resource('lifestyle');
+  this.resource('explore');
+
+
+
+  });
