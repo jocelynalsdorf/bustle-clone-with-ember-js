c8a17ea97ede806e88e201b89c9c6d03dc6edcb1
jocelynalsdorf <jfrodsla@gmail.com>
2015-06-09 09:13:31 -0700

    adds functionality to controllers for 4 base articles on homepage

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -1,3 +1,25 @@
 Bustle.HomepageController = Ember.Controller.extend({
-
-})
+	actions: {	
+		showStory: function() {	
+			$(".main").addClass('hidden');
+			$(".article-full.first-art").removeClass('hidden');
+		},
+		showStory2: function() {	
+			$(".main").addClass('hidden');
+			$(".article-full.second-art").removeClass('hidden');
+		},
+		showStory3: function() {	
+			$(".main").addClass('hidden');
+			$(".article-full.third-art").removeClass('hidden');
+		},
+		showStory4: function() {	
+			$(".main").addClass('hidden');
+			$(".article-full.fourth-art").removeClass('hidden');
+		},
+		closeStory: function() {
+			
+			$(".main").removeClass('hidden');
+			$(".article-full").addClass('hidden');
+		}
+	}
+});
